edd1b13249efbb31b55328bce1ca6c2026f9f645
danielboettich <danielboettich@users.noreply.github.com>
2020-06-25 17:45:27 +0200

    daws

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 $(function scrolly(){
-    //Keep track of last scroll
-    var lastScroll = 0;
-    $(window).scroll(function(event){
-        //Sets the current scroll position
-        var st = $(this).scrollTop();
-        //Determines up-or-down scrolling
-        if (st > lastScroll){
-           //Replace this with your function call for downward-scrolling
-          //Maybe some timeout?
-         // setTimeout( function () {  
-          $('nav').addClass('fixedAtTop');
-         // }, 800);
-        }
-        else {
-           //Replace this with your function call for upward-scrolling
-          
-          $('nav').removeClass('fixedAtTop');
-          
-        }
-        //Updates scroll position
-        lastScroll = st;
-    });
-  $('nav').hover(function(){
-    $(this).removeClass('fixedAtTop');
+  //Keep track of last scroll
+  var lastScroll = 0;
+  $(window).scroll(function(event){
+      //Sets the current scroll position
+      var st = $(this).scrollTop();
+      //Determines up-or-down scrolling
+      if (st > lastScroll){
+         //Replace this with your function call for downward-scrolling
+        //Maybe some timeout?
+       // setTimeout( function () {  
+        $('nav').addClass('fixedAtTop');
+       // }, 800);
+      }
+      else {
+         //Replace this with your function call for upward-scrolling
+        
+        $('nav').removeClass('fixedAtTop');
+        
+      }
+      //Updates scroll position
+      lastScroll = st;
   });
+$('nav').hover(function(){
+  $(this).removeClass('fixedAtTop');
+});
 });
 
 
